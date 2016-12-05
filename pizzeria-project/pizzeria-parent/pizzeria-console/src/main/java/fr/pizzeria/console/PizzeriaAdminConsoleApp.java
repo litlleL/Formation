1cc9ab2f0b6c00129b9b9b1f0040404bb96f6e7b
Livio Asdrubal <asdrubal.livio@gmail.com>
2016-12-05 16:51:14 +0100

    Tp Maven + Mise en place du filtering et de profil dans Maven pour le changement d'une variable

--- pizzeria-project/pizzeria-parent/pizzeria-console/src/main/java/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
+++ pizzeria-project/pizzeria-parent/pizzeria-console/src/main/java/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
@@ -1,8 +1,9 @@
 package fr.pizzeria.console;
 
+import java.util.ResourceBundle;
 import java.util.Scanner;
 
-import fr.pizzeria.dao.service.PizzaFactoryFichier;
+import fr.pizzeria.dao.service.PizzaDaoFactory;
 import fr.pizzeria.ihm.IhmUtil;
 import fr.pizzeria.ihm.Menu;
 import fr.pizzeria.model.Pizza;
@@ -16,7 +17,22 @@ public class PizzeriaAdminConsoleApp {
 		p2.setCode("MAR");
 
 		System.out.println("p1=p2 ? " + p1.equals(p2));
-		IhmUtil ihmUtil = new IhmUtil(new Scanner(System.in), new PizzaFactoryFichier());
+
+		ResourceBundle bundle = ResourceBundle.getBundle("application");
+		String daoImpl = bundle.getString("dao.impl");
+
+		PizzaDaoFactory daoFactory = null;
+		try {
+			daoFactory = (PizzaDaoFactory) Class.forName(daoImpl).newInstance();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		IhmUtil ihmUtil = new IhmUtil(new Scanner(System.in), daoFactory);
 		Menu menu = new Menu(ihmUtil);
 		menu.start();
 	}
